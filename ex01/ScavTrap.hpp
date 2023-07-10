1615f896e15e53efc00d9ab9678a94127bc15b70
jaiveca- <jaiveca-@student.42lisboa.com>
2023-07-10 22:53:23 +0100

    ex00 done

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaiveca- <jaiveca-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/07 19:01:26 by jaiveca-          #+#    #+#             */
-/*   Updated: 2023/07/10 22:48:18 by jaiveca-         ###   ########.fr       */
+/*   Created: 2023/07/10 22:51:44 by jaiveca-          #+#    #+#             */
+/*   Updated: 2023/07/10 22:51:45 by jaiveca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
-
-int	main(void)
-{
-	std::string	target = "Target";
-
-	ClapTrap clap("ClapMaster2000");
-	clap.attack(target);
-	clap.takeDamage(3);
-	clap.beRepaired(2);
-	for (int i = 0; i < 9; i++)
-		clap.attack(target);
-	
-	std::cout << std::endl;
-	
-	ClapTrap clappy("ClapMeister3000");
-	clappy.takeDamage(42);
-	clappy.attack(target);
-
-	std::cout << std::endl;
-}
